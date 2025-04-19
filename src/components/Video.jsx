7b57155d07a80e4443f46244e41141620ae24781
M Kazim <mkaazim13@gmail.com>
2025-04-20 03:16:26 +0500

    add styles

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,16 +1,16 @@
 function Video({ src, poster }) {
-    return (
-      <div className="relative w-full bg-black rounded-md overflow-hidden">
-        <video
-          src={src}
-          poster={poster}
-          autoPlay
-          controls
-          playsInline
-          className="sm:h-[68vh] sm:max-w-4xl h-64 w-full object-contain mx-auto"
-        ></video>
-      </div>
-    )
-  }
+  return (
+    <div className="relative w-full bg-black rounded-md overflow-hidden">
+      <video
+        src={src}
+        poster={poster}
+        autoPlay
+        controls
+        playsInline
+        className="w-full aspect-video object-contain mx-auto"
+      ></video>
+    </div>
+  )
+}
 
-  export default Video
+export default Video
