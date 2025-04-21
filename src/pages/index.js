8ec9f8d0159ecb6b1db2557a367b93d10246d6bd
M Kazim <mkaazim13@gmail.com>
2025-04-21 17:29:13 +0500

    fix case sensitivity

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import Channel from "./Channel/Channel.jsx";
+import Channel from "./Channel/Channel";
 import ChannelSubscribers from "./Channel/Subscribers";
 import ChannelTweets from "./Channel/Tweets";
 import ChannelVideos from "./Channel/Videos";
