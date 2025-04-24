8dc6c66650477f4bebbfe896349999f1b808bb5b
M Kazim <mkaazim13@gmail.com>
2025-04-24 18:34:14 +0500

    revert chnages for testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,235 +1,202 @@
-import React, { useEffect, lazy, Suspense } from "react";
+import React, { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
+import { AuthLayout, Login, SignUp } from "./components/index";
 import { Toaster } from "react-hot-toast";
 import { useDispatch, useSelector } from "react-redux";
 import { getCurrentUser } from "./store/authSlice";
-import Spinner from "./components/Spinner";
-
-// Lazy loaded pages & components
-const Home = lazy(() => import("./pages/Home"));
-const Login = lazy(() => import("./components/Login"));
-const SignUp = lazy(() => import("./components/Signup"));
-const AuthLayout = lazy(() => import("./components/AuthLayout"));
-const Layout = lazy(() => import("./components/Layout"));
-const EditPersonalInfo = lazy(() => import("./components/EditPersonalInfo"));
-const ChangePassword = lazy(() => import("./components/ChangePassword"));
-const Channel = lazy(() => import("./pages/Channel/Channel"));
-const ChannelVideos = lazy(() => import("./pages/Channel/Videos"));
-const ChannelTweets = lazy(() => import("./pages/Channel/Tweets"));
-const ChannelSubscribers = lazy(() => import("./pages/Channel/Subscribers"));
-const ChannelPlaylist = lazy(() => import("./pages/Channel/Playlist"));
-const LikedVideos = lazy(() => import("./pages/LikedVideos"));
-const History = lazy(() => import("./pages/History"));
-const VideoDetail = lazy(() => import("./pages/VideoDetail"));
-const AdminDashboard = lazy(() => import("./pages/AdminDashboard"));
-const EditChannel = lazy(() => import("./pages/EditChannel"));
-const PlaylistVideos = lazy(() => import("./pages/PlaylistVideos"));
-const SearchVideos = lazy(() => import("./pages/SearchVideos"));
-const Subscriptions = lazy(() => import("./pages/Subscriptions"));
-const TermsAndConditions = lazy(() => import("./pages/TermsAndConditions"));
+import Spinner  from './components/Spinner';
+import {
+  History,
+  Channel,
+  ChannelVideos,
+  ChannelTweets,
+  LikedVideos,
+  VideoDetail,
+  ChannelSubscribers,
+  Subscriptions,
+  AdminDashboard,
+  EditChannel,
+  Home,
+  SearchVideos,
+  TermsAndConditions,
+  ChannelPlaylist,
+  PlaylistVideos
+} from "./pages";
+import { EditPersonalInfo, ChangePassword, Layout } from "./components";
 
 function App() {
   const dispatch = useDispatch();
-  const { loading } = useSelector((state) => state.auth);
+  const { loading, status } = useSelector(state => state.auth);
 
   useEffect(() => {
     dispatch(getCurrentUser());
   }, [dispatch]);
 
+  if (loading) {
+    return <Spinner />;
+  }
+
   return (
     <>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <Suspense fallback={<Spinner />}>
-              <Layout />
-            </Suspense>
-          }
-        >
-          {/* Public Routes */}
+        <Route path="/" element={<Layout />}>
           <Route
-            index
+            path=""
             element={
-              <Suspense fallback={<Spinner />}>
-                <AuthLayout authentication={false}>
-                  <Home />
-                </AuthLayout>
-              </Suspense>
+              <AuthLayout authentication={false}>
+                <Home />
+              </AuthLayout>
             }
           />
           <Route
             path="/search/:query"
             element={
-              <Suspense fallback={<Spinner />}>
-                <AuthLayout authentication={false}>
-                  <SearchVideos />
-                </AuthLayout>
-              </Suspense>
+              <AuthLayout authentication={false}>
+                <SearchVideos />
+              </AuthLayout>
             }
           />
-
-          {/* Protected Routes */}
           <Route
             path="/channel/:username"
             element={
-              <Suspense fallback={<Spinner />}>
               <AuthLayout authentication={true}>
-
-                  <Channel />
-
+                <Channel />
               </AuthLayout>
-              </Suspense>
             }
           >
             <Route
               path="videos"
               element={
-                <Suspense fallback={<Spinner />}>
+                <AuthLayout authentication={true}>
                   <ChannelVideos />
-                </Suspense>
+                </AuthLayout>
               }
             />
             <Route
               path="playlists"
               element={
-                <Suspense fallback={<Spinner />}>
+                <AuthLayout authentication={true}>
                   <ChannelPlaylist />
-                </Suspense>
+                </AuthLayout>
               }
             />
             <Route
               path="playlist/:id"
               element={
-                <Suspense fallback={<Spinner />}>
+                <AuthLayout authentication={true}>
                   <PlaylistVideos />
-                </Suspense>
+                </AuthLayout>
               }
             />
             <Route
               path="tweets"
               element={
-                <Suspense fallback={<Spinner />}>
+                <AuthLayout authentication={true}>
                   <ChannelTweets />
-                </Suspense>
+                </AuthLayout>
               }
             />
             <Route
               path="subscribed"
               element={
-                <Suspense fallback={<Spinner />}>
+                <AuthLayout authentication={false}>
                   <ChannelSubscribers />
-                </Suspense>
+                </AuthLayout>
               }
             />
           </Route>
-
           <Route
             path="/history"
             element={
-              <Suspense fallback={<Spinner />}>
-                <AuthLayout authentication={true}>
-                  <History />
-                </AuthLayout>
-              </Suspense>
+              <AuthLayout authentication={true}>
+                <History />
+              </AuthLayout>
             }
           />
           <Route
             path="/liked-videos"
             element={
-              <Suspense fallback={<Spinner />}>
-                <AuthLayout authentication={true}>
-                  <LikedVideos />
-                </AuthLayout>
-              </Suspense>
+              <AuthLayout authentication={true}>
+                <LikedVideos />
+              </AuthLayout>
             }
           />
           <Route
-            path="/subscriptions"
-            element={
-              <Suspense fallback={<Spinner />}>
-                <AuthLayout authentication={true}>
-                <Subscriptions />
-
-                </AuthLayout>
-              </Suspense>
-            }
-          />
-          <Route
-            path="/collections"
-            element={
-              <Suspense fallback={<Spinner />}>
-                <AuthLayout authentication={true}>
-                <AdminDashboard />
-                </AuthLayout>
-              </Suspense>
-            }
-          />
+          path="/subscriptions"
+          element={
+            <AuthLayout authentication={true}>
+              <Subscriptions />
+            </AuthLayout>
+          }
+        />
           <Route
             path="/edit"
             element={
-              <Suspense fallback={<Spinner />}>
-                <AuthLayout authentication={true}>
-                  <EditChannel />
-                </AuthLayout>
-              </Suspense>
+              <AuthLayout authentication={true}>
+                <EditChannel />
+              </AuthLayout>
             }
           >
             <Route
               path="personalInfo"
               element={
-                <Suspense fallback={<Spinner />}>
+                <AuthLayout authentication={true}>
                   <EditPersonalInfo />
-                </Suspense>
+                </AuthLayout>
               }
             />
             <Route
               path="password"
               element={
-                <Suspense fallback={<Spinner />}>
+                <AuthLayout authentication={true}>
                   <ChangePassword />
-                </Suspense>
+                </AuthLayout>
               }
             />
           </Route>
         </Route>
-
-        {/* Auth and misc routes outside layout */}
         <Route
           path="/login"
           element={
-            <Suspense fallback={<Spinner />}>
+            <AuthLayout authentication={false}>
               <Login />
-            </Suspense>
+            </AuthLayout>
           }
         />
         <Route
           path="/signup"
           element={
-            <Suspense fallback={<Spinner />}>
+            <AuthLayout authentication={false}>
               <SignUp />
-            </Suspense>
+            </AuthLayout>
           }
         />
         <Route
           path="/watch/:videoId"
           element={
-            <Suspense fallback={<Spinner />}>
+            <AuthLayout authentication={true}>
               <VideoDetail />
-            </Suspense>
+            </AuthLayout>
+          }
+        />
+        <Route
+          path="/collections"
+          element={
+            <AuthLayout authentication={true}>
+              <AdminDashboard />
+            </AuthLayout>
           }
         />
         <Route
           path="/terms&conditions"
           element={
-            <Suspense fallback={<Spinner />}>
+            <AuthLayout authentication={true}>
               <TermsAndConditions />
-            </Suspense>
+            </AuthLayout>
           }
         />
       </Routes>
 
-      {/* Toast Notifications */}
       <Toaster
         position="top-right"
         reverseOrder={true}
