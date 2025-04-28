df8eee384d8ecc99637592996a3b0a3672001f2c
M Kazim <mkaazim13@gmail.com>
2025-04-29 02:19:52 +0500

    add lazyLoading for faster initial load

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,32 @@
-import React, { useEffect } from "react";
+import React, { useEffect, lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import { AuthLayout, Login, SignUp } from "./components/index";
 import { Toaster } from "react-hot-toast";
 import { useDispatch, useSelector } from "react-redux";
 import { getCurrentUser } from "./store/authSlice";
 import Spinner from "./components/Spinner";
-import {
-  History,
-  Channel,
-  ChannelVideos,
-  ChannelTweets,
-  LikedVideos,
-  VideoDetail,
-  ChannelSubscribers,
-  Subscriptions,
-  AdminDashboard,
-  EditChannel,
-  Home,
-  SearchVideos,
-  TermsAndConditions,
-  ChannelPlaylist,
-  PlaylistVideos,
-} from "./pages";
 import { EditPersonalInfo, ChangePassword, Layout } from "./components";
+
+// Lazy loading
+const History = lazy(() => import("./pages/History"));
+const Channel = lazy(() => import("./pages/Channel/Channel"));
+const ChannelVideos = lazy(() => import("./pages/Channel/Videos"));
+const ChannelTweets = lazy(() => import("./pages/Channel/Tweets"));
+const LikedVideos = lazy(() => import("./pages/LikedVideos"));
+const VideoDetail = lazy(() => import("./pages/VideoDetail"));
+const ChannelSubscribers = lazy(() => import("./pages/Channel/Subscribers"));
+const Subscriptions = lazy(() => import("./pages/Subscriptions"));
+const AdminDashboard = lazy(() => import("./pages/AdminDashboard"));
+const EditChannel = lazy(() => import("./pages/EditChannel"));
+const Home = lazy(() => import("./pages/Home"));
+const SearchVideos = lazy(() => import("./pages/SearchVideos"));
+const TermsAndConditions = lazy(() => import("./pages/TermsAndConditions"));
+const ChannelPlaylist = lazy(() => import("./pages/Channel/Playlist"));
+const PlaylistVideos = lazy(() => import("./pages/PlaylistVideos"));
 
 function App() {
   const dispatch = useDispatch();
-  const { loading, status } = useSelector((state) => state.auth);
+  const { loading } = useSelector((state) => state.auth);
 
   useEffect(() => {
     dispatch(getCurrentUser());
@@ -38,170 +38,174 @@ function App() {
 
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route
-            path=""
-            element={
-              <AuthLayout authentication={false}>
-                <Home />
-              </AuthLayout>
-            }
-          />
-          <Route
-            path="/search/:query"
-            element={
-              <AuthLayout authentication={false}>
-                <SearchVideos />
-              </AuthLayout>
-            }
-          />
-          <Route
-            path="/channel"
-            element={
-              <AuthLayout authentication={true}>{/* <Channel /> */}</AuthLayout>
-            }
-          />
-          <Route
-            path="/channel/:username"
-            element={
-              <AuthLayout authentication={true}>
-                <Channel />
-              </AuthLayout>
-            }
-          >
+      <Suspense fallback={<Spinner />}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
             <Route
-              path="videos"
-              element={
-                <AuthLayout authentication={true}>
-                  <ChannelVideos />
-                </AuthLayout>
-              }
-            />
-            <Route
-              path="playlists"
-              element={
-                <AuthLayout authentication={true}>
-                  <ChannelPlaylist />
-                </AuthLayout>
-              }
-            />
-            <Route
-              path="playlist/:id"
-              element={
-                <AuthLayout authentication={true}>
-                  <PlaylistVideos />
-                </AuthLayout>
-              }
-            />
-            <Route
-              path="tweets"
-              element={
-                <AuthLayout authentication={true}>
-                  <ChannelTweets />
-                </AuthLayout>
-              }
-            />
-            <Route
-              path="subscribed"
+              path=""
               element={
                 <AuthLayout authentication={false}>
-                  <ChannelSubscribers />
-                </AuthLayout>
-              }
-            />
-          </Route>
-          <Route
-            path="/history"
-            element={
-              <AuthLayout authentication={true}>
-                <History />
-              </AuthLayout>
-            }
-          />
-          <Route
-            path="/liked-videos"
-            element={
-              <AuthLayout authentication={true}>
-                <LikedVideos />
-              </AuthLayout>
-            }
-          />
-          <Route
-            path="/subscriptions"
-            element={
-              <AuthLayout authentication={true}>
-                <Subscriptions />
-              </AuthLayout>
-            }
-          />
-          <Route
-            path="/edit"
-            element={
-              <AuthLayout authentication={true}>
-                <EditChannel />
-              </AuthLayout>
-            }
-          >
-            <Route
-              path="personalInfo"
-              element={
-                <AuthLayout authentication={true}>
-                  <EditPersonalInfo />
+                  <Home />
                 </AuthLayout>
               }
             />
             <Route
-              path="password"
+              path="/search/:query"
               element={
-                <AuthLayout authentication={true}>
-                  <ChangePassword />
+                <AuthLayout authentication={false}>
+                  <SearchVideos />
                 </AuthLayout>
               }
             />
+            <Route
+              path="/channel"
+              element={
+                <AuthLayout authentication={true}>
+                  {/* Empty for now */}
+                </AuthLayout>
+              }
+            />
+            <Route
+              path="/channel/:username"
+              element={
+                <AuthLayout authentication={true}>
+                  <Channel />
+                </AuthLayout>
+              }
+            >
+              <Route
+                path="videos"
+                element={
+                  <AuthLayout authentication={true}>
+                    <ChannelVideos />
+                  </AuthLayout>
+                }
+              />
+              <Route
+                path="playlists"
+                element={
+                  <AuthLayout authentication={true}>
+                    <ChannelPlaylist />
+                  </AuthLayout>
+                }
+              />
+              <Route
+                path="playlist/:id"
+                element={
+                  <AuthLayout authentication={true}>
+                    <PlaylistVideos />
+                  </AuthLayout>
+                }
+              />
+              <Route
+                path="tweets"
+                element={
+                  <AuthLayout authentication={true}>
+                    <ChannelTweets />
+                  </AuthLayout>
+                }
+              />
+              <Route
+                path="subscribed"
+                element={
+                  <AuthLayout authentication={false}>
+                    <ChannelSubscribers />
+                  </AuthLayout>
+                }
+              />
+            </Route>
+            <Route
+              path="/history"
+              element={
+                <AuthLayout authentication={true}>
+                  <History />
+                </AuthLayout>
+              }
+            />
+            <Route
+              path="/liked-videos"
+              element={
+                <AuthLayout authentication={true}>
+                  <LikedVideos />
+                </AuthLayout>
+              }
+            />
+            <Route
+              path="/subscriptions"
+              element={
+                <AuthLayout authentication={true}>
+                  <Subscriptions />
+                </AuthLayout>
+              }
+            />
+            <Route
+              path="/edit"
+              element={
+                <AuthLayout authentication={true}>
+                  <EditChannel />
+                </AuthLayout>
+              }
+            >
+              <Route
+                path="personalInfo"
+                element={
+                  <AuthLayout authentication={true}>
+                    <EditPersonalInfo />
+                  </AuthLayout>
+                }
+              />
+              <Route
+                path="password"
+                element={
+                  <AuthLayout authentication={true}>
+                    <ChangePassword />
+                  </AuthLayout>
+                }
+              />
+            </Route>
           </Route>
-        </Route>
-        <Route
-          path="/login"
-          element={
-            <AuthLayout authentication={false}>
-              <Login />
-            </AuthLayout>
-          }
-        />
-        <Route
-          path="/signup"
-          element={
-            <AuthLayout authentication={false}>
-              <SignUp />
-            </AuthLayout>
-          }
-        />
-        <Route
-          path="/watch/:videoId"
-          element={
-            <AuthLayout authentication={true}>
-              <VideoDetail />
-            </AuthLayout>
-          }
-        />
-        <Route
-          path="/collections"
-          element={
-            <AuthLayout authentication={true}>
-              <AdminDashboard />
-            </AuthLayout>
-          }
-        />
-        <Route
-          path="/terms&conditions"
-          element={
-            <AuthLayout authentication={true}>
-              <TermsAndConditions />
-            </AuthLayout>
-          }
-        />
-      </Routes>
+          <Route
+            path="/login"
+            element={
+              <AuthLayout authentication={false}>
+                <Login />
+              </AuthLayout>
+            }
+          />
+          <Route
+            path="/signup"
+            element={
+              <AuthLayout authentication={false}>
+                <SignUp />
+              </AuthLayout>
+            }
+          />
+          <Route
+            path="/watch/:videoId"
+            element={
+              <AuthLayout authentication={true}>
+                <VideoDetail />
+              </AuthLayout>
+            }
+          />
+          <Route
+            path="/collections"
+            element={
+              <AuthLayout authentication={true}>
+                <AdminDashboard />
+              </AuthLayout>
+            }
+          />
+          <Route
+            path="/terms&conditions"
+            element={
+              <AuthLayout authentication={true}>
+                <TermsAndConditions />
+              </AuthLayout>
+            }
+          />
+        </Routes>
+      </Suspense>
 
       <Toaster
         position="top-right"
